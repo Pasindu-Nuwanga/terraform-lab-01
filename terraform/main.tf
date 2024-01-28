e6099fd2bc301c7031e26c3278ecb8dab16d0242
Pasindu Nuwanga <pasindunuwanga3@gmail.com>
2024-01-28 17:27:55 +0530

    updated 1/28/2024

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,14 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "first-iac-instance" {
-    ami = "ami-0a3c3a20c09d6f377"
-    instance_type = "t2.micro"
+  ami           = "ami-0a3c3a20c09d6f377"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "my-first-ec2"
+    Environment = "dev"
+  }
 }
+
